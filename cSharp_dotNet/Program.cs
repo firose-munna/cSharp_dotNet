cb31c37cc256411522dd27e902ffeac49a093383
firose-munna <munnafirose@gmail.com>
2024-03-06 20:30:06 +0600

    section3 is done

--- cSharp_dotNet/Program.cs
+++ cSharp_dotNet/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cSharp_dotNet.calculator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace cSharp_dotNet
 {
+    //This is the another class outsite the Main Class
     public class WelcomeClass
     {
         public string welcome = "Hello, My name is Firose Munna. I am a Trainee Software Engineer of BSS.";
@@ -16,6 +18,7 @@ namespace cSharp_dotNet
         }
     }
 
+    //This is also the another class outsite the Main class....
     public class DataType
     {
         public int number1;
@@ -39,7 +42,6 @@ namespace cSharp_dotNet
 
 
             //This is second class... primitive data type 
-
             DataType data1 = new DataType();
             data1.number1 = 45;
             data1.number2 = 455.45f;
@@ -47,7 +49,49 @@ namespace cSharp_dotNet
 
             data1.showData();
 
+           // this is the range 
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
 
+
+            //this is the constant value which is  not reassignable.....
+            Console.WriteLine("This is the constant value-");
+            const int number = 45;
+            const float Pi = 3.1416f;
+
+            Console.WriteLine("the integer const number is : " + number + " and the float consttnt number is : " + Pi);
+
+            //convertion the value
+            string textNumber = "12345";
+            int number3 = int.Parse(textNumber);
+            Console.WriteLine("The Converted Number is : " + number3);
+
+            int number4 = Convert.ToInt32(textNumber);
+            Console.WriteLine("This is the another way to change the text number : " + number4);
+
+            //this is the try catch block
+            try
+            {
+                byte number5 = Convert.ToByte(textNumber);
+                Console.WriteLine(number5);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("It will not to convert. Because there are a exception is occured.");
+            }
+
+
+            //Arithmetic Operator....
+            int num1 = 50;
+            int numb2 = 13;
+
+            Console.WriteLine(num1 + numb2);
+            Console.WriteLine(num1 - numb2);
+            Console.WriteLine(num1 * numb2);
+            Console.WriteLine(num1 / numb2);
+            Console.WriteLine((float)num1 / (float)numb2);
+
+            int result = Calculate.Add(num1, numb2);
+            Console.WriteLine("This Add Method is define the outsite the Program.cs file : " + result);
 
 
 
