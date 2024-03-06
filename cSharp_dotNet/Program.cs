c573ae35aa009b357b86587df70178a08c641b87
firose-munna <munnafirose@gmail.com>
2024-03-06 17:36:35 +0600

    Add project files.

---
+++ cSharp_dotNet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_dotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I am Firose Munna. I am Trainee Software Engineer of BSS.");
+            Console.ReadLine();
+        }
+    }
+}
