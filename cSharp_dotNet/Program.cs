6c2f192b4e168ed0deb79ca7fa5d5f6b3bf4a353
firose-munna <munnafirose@gmail.com>
2024-03-07 16:44:38 +0600

    section5- control flow done

--- cSharp_dotNet/Program.cs
+++ cSharp_dotNet/Program.cs
@@ -8,30 +8,7 @@ using System.Threading.Tasks;
 
 namespace cSharp_dotNet
 {
-    //This is the another class outsite the Main Class
-    public class WelcomeClass
-    {
-        public string welcome = "Hello, My name is Firose Munna. I am a Trainee Software Engineer of BSS.";
-
-        public void ShowMessage()
-        {
-            Console.WriteLine(welcome);
-        }
-    }
-
-    //This is also the another class outsite the Main class....
-    public class DataType
-    {
-        public int number1;
-        public float number2;
-        public char letter;
-        public void showData() {
-            Console.WriteLine("The Integer Valu is : " + number1);
-            Console.WriteLine("The Float Value is : " + number2);
-            Console.WriteLine("The Char is : " + letter);
-        }
-    }
-    
+   
 
 
     internal class Program
@@ -129,6 +106,50 @@ namespace cSharp_dotNet
             Console.WriteLine();
             ValueReference();
 
+            //Conditional statement
+            Console.WriteLine();
+            Conditional();
+
+
+            //excercise 1 
+            Console.WriteLine();
+            Excercise1();
+
+            //excercise 2
+            Console.WriteLine();
+            int n1;
+            int n2;
+
+            Console.WriteLine("Enter the first number - ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number - ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            int r = MaxNumber(n1, n2);
+
+            Console.WriteLine("Maximum number of these two number is : " + r);
+
+            //Excercise 3
+            ImageSize();
+
+            //Iteration
+            Console.WriteLine();
+            Console.WriteLine("loops-");
+            Iteration();
+
+
+
+            //Random PassWord
+            Console.WriteLine();
+            Console.WriteLine("Random Password - ");
+
+            RandomNumber();
+
+
+            //Practice 1
+            Console.WriteLine();
+            Practice1();
+
 
             Console.ReadLine();
         }
@@ -150,5 +171,212 @@ namespace cSharp_dotNet
 
             Console.WriteLine(string.Format("Numbers[0] : {0} and Numbers2[0] : {1}.", numbers[0], numbers2[0]));
         }
+
+        static void Conditional()
+        {
+            int score = 5;
+            string status;
+            string grade;
+
+
+            if(score >=80 && score <= 100)
+            {
+                status = "Pass";
+                grade = "A+";
+            }
+            else if (score >= 70 && score < 80)
+            {
+                status = "Pass";
+                grade = "A";
+            }
+            else if (score >= 60 && score < 70)
+            {
+                status = "Pass";
+                grade = "A-";
+            }
+            else if (score >= 50 && score < 60)
+            {
+                status = "Pass";
+                grade = "B";
+            }
+            else
+            {
+                status = "Fail";
+                grade = "F";
+            }
+
+            Console.WriteLine(string.Format("Using If-Else Statement....Congrats, You are {0} and the Grade is {1}.", status, grade));
+
+
+
+            string ternaryStatus = (score >=50) ? "Passed" : "Failed";
+            Console.WriteLine(string.Format("Using Ternary Statement....Congrats, You are {0}.", ternaryStatus));
+
+
+
+            int day = 5;
+
+            switch(day)
+            {
+                case 1:
+                    Console.WriteLine("Saterday");
+                    break;
+                case 2:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 3:
+                    Console.WriteLine("Monday");
+                    break ; 
+                case 4:
+                    Console.WriteLine("TuesDay");
+                    break;
+                case 5:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 6:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 7:
+                    Console.WriteLine("Friday" );
+                    break;
+
+                default:
+                    Console.WriteLine("No day available");
+                    break;
+
+            }
+
+        }
+
+        static void Excercise1()
+        {
+            
+            Console.WriteLine("Enter a number berween 1 to 10-");
+            int number =  Convert.ToInt32((string) Console.ReadLine());
+
+            string output = (number>=1 && number<=10) ? "Valid" : "Invalid";
+
+            Console.WriteLine("The User Input is : " + output);
+
+            
+        }
+
+        static int MaxNumber(int n1, int n2)
+        {
+            if (n1 > n2) { 
+                return n1;
+            }
+            else
+            {
+                return n2;
+            }
+        }
+
+        static void ImageSize()
+        {
+            Console.WriteLine("Enter the Width of the image-");
+            int w = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Height of the image-");
+            int h = Convert.ToInt32(Console.ReadLine());
+
+            if(w > h)
+            {
+                Console.WriteLine("The Image is LandScape.");
+            }
+            else if(w < h)
+            {
+                Console.WriteLine("The Image id Portrait");
+            }
+            else
+            {
+                Console.WriteLine("Image is Square Size");
+            }
+        }
+
+
+        static void Iteration()
+        {
+            //For Loop
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(" " + i);
+            }
+            Console.WriteLine();
+
+            int[] numbers = new int[10] { 56,45,48,12,11,9,36,46,89,66};
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            while (true)
+            {
+                string name;
+                Console.Write("Enter your name : ");
+                name = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(name))
+                {
+                    break;
+                }
+                Console.WriteLine("Your name : " + name);
+            }
+        }
+
+        static void RandomNumber()
+        {
+            const int length = 20;
+            var random = new Random();
+            var buffer = new char[length];
+
+            for( int i = 0; i < length; i++ )
+            {
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            }
+
+            var password = new string(buffer);
+
+            Console.WriteLine("The Password is : " + password);
+        }
+
+        static void Practice1()
+        {
+            int count = 0;
+
+            for(int i = 1; i<=100; i++)
+            {
+                if(i%3 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Total number of divisible by 3 : " +count);
+        }
     }
+    //This is the another class outsite the Main Class
+    public class WelcomeClass
+    {
+        public string welcome = "Hello, My name is Firose Munna. I am a Trainee Software Engineer of BSS.";
+
+        public void ShowMessage()
+        {
+            Console.WriteLine(welcome);
+        }
+    }
+
+    //This is also the another class outsite the Main class....
+    public class DataType
+    {
+        public int number1;
+        public float number2;
+        public char letter;
+        public void showData()
+        {
+            Console.WriteLine("The Integer Valu is : " + number1);
+            Console.WriteLine("The Float Value is : " + number2);
+            Console.WriteLine("The Char is : " + letter);
+        }
+    }
+
 }
