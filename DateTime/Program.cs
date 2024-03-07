1a0703fdfebc20a14cf1279ce93e6c77ad5d09c2
firose-munna <munnafirose@gmail.com>
2024-03-07 19:16:41 +0600

    date and time section 7 is done

---
+++ DateTime/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateandTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var now = DateTime.Now;
+
+            Console.WriteLine("Current DateTime is : "+ now);
+
+            Console.WriteLine("Hour : " + now.Hour);
+            Console.WriteLine("Minutes : " + now.Minute);
+
+            Console.WriteLine("Custom formate : " + now.ToString("dd.MM.yyyy"));
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+
+
+            var tomorrow = now.AddDays(1);
+            Console.WriteLine(tomorrow);
+
+            var newdate = new DateTime(2024, 3, 7);
+            newdate.AddYears(1);
+
+            Console.WriteLine(newdate.Year);
+
+            Console.WriteLine(TimeSpan.FromHours(1));
+
+            Console.ReadLine();  
+        }
+    }
+}
